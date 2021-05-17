1de234b6cb692054d93e0eead9359682b896fdf6
siewertsen1999 <73901609+siewertsen1999@users.noreply.github.com>
2021-05-17 18:15:10 +0200

    Update Chessboard.java

--- Chess/src/Chessboard.java
+++ Chess/src/Chessboard.java
@@ -7,16 +7,7 @@ public class Chessboard {
         this.chessboard = new Character[8][8];
     }
 
-    // this method will only be executed if the move funktion of the Character returns true
-   /* public void move(char xOld, int yOld, char xNew, int yNew){    // x -> A-H on board,y -> 1-8 on board, board as third parameter always has to be the chessboard
-        int posXOld = (int)toUpperCase(xOld) - 48;
-        int posXNew = (int)toUpperCase(xNew) - 48;
-        chessboard[posXNew][yNew] = chessboard[posXOld][yOld];
-        chessboard[posXOld][yOld] = null;
-        //chessboard.pos
-    }
-
-    */
+ 
     public void move( int xOld, int yOld, int xNew, int yNew) {    // x -> A-H on board,y -> 1-8 on board, board as third parameter always has to be the chessboard
         //int posXOld = (int) toUpperCase(xOld) - 65;
         //int posXNew = (int) toUpperCase(xNew) - 65;
